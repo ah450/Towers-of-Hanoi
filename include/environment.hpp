490d20744b77a2ab067851195c3fac171a554946
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2014-01-03 18:51:40 +0200

    game interface

--- include/environment.hpp
+++ include/environment.hpp
@@ -6,14 +6,27 @@
 #include <stack>
 #include <array>
 
+enum class rod_selected{
+    left, middle, right;
+};
+
+enum class direction{
+    up, down, left, right, in, out;
+};
+
 class Environment{
 private:
     Land land;
     std::array<Rod, 3> rods;
     std::array<Disk, 8> disks;
     std::array<std::stack<Disk *>, 3> stacks_array;
-    
+    bool current_selected;
+    Disk * selected;
 public:
     Environment();
     void draw();
+    bool select(rod_selected);
+    void move(direction);
+    void unselect();
+    void highLight(rod_selected);
 };
